2cc6f0a273c285541896c1d26c89fc0853957551
Tim Lee <timothy.lee@intlfcstone.com>
2018-08-12 00:35:38 -0400

    Added first ProfileViewModel

---
+++ mvvm/ProfileViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileViewModel.swift
+//  mvvm
+//
+//  Created by Timothy Lee on 8/11/18.
+//  Copyright © 2018 Timothy Lee. All rights reserved.
+//
+
+import Foundation
+
+enum ProfileViewModelItemType {
+    case nameAndPicture
+    case about
+    case email
+    case friend
+    case attribute
+}
+
+protocol ProfileViewModelItem {
+    var type: ProfileViewModelItemType { get }
+    var rowCount: Int { get }
+    var sectionTitle: String { get }
+}
+
+extension ProfileViewModelItem {
+    var rowCount: Int {
+        return 1
+    }
+}
+
+class ProfileViewModelNameItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .nameAndPicture
+    }
+    
+    var sectionTitle: String {
+        return "Main Info"
+    }
+}
