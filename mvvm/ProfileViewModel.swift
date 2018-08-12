74e6b8930a5d8da30af352262a2a0039d7ffab72
Tim Lee <timothy.lee@intlfcstone.com>
2018-08-12 01:16:40 -0400

    Added ProfileViewModel class

--- mvvm/ProfileViewModel.swift
+++ mvvm/ProfileViewModel.swift
@@ -22,6 +22,42 @@ protocol ProfileViewModelItem {
     var sectionTitle: String { get }
 }
 
+class ProfileViewModel: NSObject {
+    var items = [ProfileViewModelItem]()
+    
+    override init() {
+        super.init()
+        guard let data = dataFromFile("ServerData"), let profile = Profile(data: data) else { return }
+        if let name = profile.fullName, let pictureUrl = profile.pictureUrl {
+            let nameAndPictureItem = ProfileViewModelNameAndPictureItem(pictureUrl: pictureUrl, userName: name)
+            items.append(nameAndPictureItem)
+        }
+        
+        if let about = profile.about {
+            let aboutItem = ProfileViewModelAboutItem(about: about)
+            items.append(aboutItem)
+        }
+        
+        if let email = profile.email {
+            let dobItem = ProfileViewModelEmailItem(email: email)
+            items.append(dobItem)
+        }
+        
+        let attributes = profile.profileAttributes
+        if !attributes.isEmpty {
+            let attributesItem = ProfileViewModelAttributeItem(attributes: attributes)
+            items.append(attributesItem)
+        }
+        
+        let friends = profile.friends
+        if !profile.friends.isEmpty {
+            let friendsItem = ProfileViewModelFriendsItem(friends: friends)
+            items.append(friendsItem)
+        }
+    }
+}
+
+
 extension ProfileViewModelItem {
     var rowCount: Int {
         return 1
@@ -37,3 +73,118 @@ class ProfileViewModelNameItem: ProfileViewModelItem {
         return "Main Info"
     }
 }
+
+class ProfileViewModelNameAndPictureItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .nameAndPicture
+    }
+    
+    var sectionTitle: String {
+        return "Main Info"
+    }
+    
+    var pictureUrl: String
+    var userName: String
+    
+    init(pictureUrl: String, userName: String) {
+        self.pictureUrl = pictureUrl
+        self.userName = userName
+    }
+}
+
+class ProfileViewModelAboutItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .about
+    }
+    
+    var sectionTitle: String {
+        return "About"
+    }
+    
+    var about: String
+    init(about: String) {
+        self.about = about
+    }
+}
+
+class ProfileViewModelEmailItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .email
+    }
+    
+    var sectionTitle: String {
+        return "Email"
+    }
+    
+    var email: String
+    init(email: String) {
+        self.email = email
+    }
+}
+
+class ProfileViewModelAttributeItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .attribute
+    }
+    
+    var sectionTitle: String {
+        return "Attributes"
+    }
+    
+    var attributes: [Attribute]
+    init(attributes: [Attribute]) {
+        self.attributes = attributes
+    }
+}
+
+class ProfileViewModelFriendsItem: ProfileViewModelItem {
+    var type: ProfileViewModelItemType {
+        return .friend
+    }
+    
+    var sectionTitle: String {
+        return "Friends"
+    }
+    
+    var rowCount: Int {
+        return friends.count
+    }
+    
+    var friends: [Friend]
+    init(friends: [Friend]) {
+        self.friends = friends
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
